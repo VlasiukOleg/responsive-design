8dab6df825219bce550005d33c39a16ff30306db
Oleg <ikslprint@gmail.com>
2024-01-04 22:09:03 +0200

    add mobile-menu

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,10 @@
 const menuBtnRef = document.querySelector(".menu-button");
+const mobileMenuRef = document.querySelector(".mobile-menu");
 
 menuBtnRef.addEventListener("click", () => {
   const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-  console.log(expanded);
   menuBtnRef.classList.toggle("is-open");
+  mobileMenuRef.classList.toggle("is-open");
 
   menuBtnRef.setAttribute("aria-expanded", !expanded);
 });
